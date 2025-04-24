841cf8ea3c47463cbeeeae9395109cc58f026ac3
leilalves <leilaaalvesrocha@gmail.com>
2025-04-24 15:33:10 -0300

    Commit da Leila: Menu

--- Conversor.java
+++ Conversor.java
@@ -5,21 +5,43 @@ public class Conversor{
     public static void main(String[] args) {
         //declaração de variáveis
         double cel, fah; //cel - temperatura em graus celsius; temperatura em graus fahrenheit
+        int op; //Opção do usuário
 
         //criação e instâlação do objeto de entrada
         Scanner entrada = new Scanner(System.in);
 
-        //apresentação do programa
-        System.out.println("\n\t\t\t -- Convertor -- \n");
+        do{
+            //Apresentação do programa
+            System.out.println("\n\t\t\t -- Convertor -- \n");
 
-        //Entradas
-        System.out.print("Informe a temperatura em °C: ");
-        cel = entrada.nextDouble();
+            //Menu
+            System.out.println("1. °C para °F");
+            System.out.println("2. °F para °C");
+            System.out.println("3. Sair");
 
-        //Processamento
-        fah = cel * 9 / 5 + 32;
+            //Ler a opção escolhida pelo usuário
+            System.out.print("\nOpção: ");
+            op = entrada.nextInt();
 
-        //Saída
-        System.out.println("\n" + cel + "°C = " + fah + "°F\n");
+            if(op == 1){
+
+                //Entradas
+                System.out.print("Informe a temperatura em °C: ");
+                cel = entrada.nextDouble();
+
+                //Processamento
+                fah = cel * 9 / 5 + 32;
+
+                //Saída
+                System.out.println("\n" + cel + "°C = " + fah + "°F\n");
+            }else if(op == 2){
+                //Converter Farenheit para Celsius
+                System.out.println("Em construção... Vaza...");
+            }else if(op == 3){
+                System.out.println("\nTchauzinho :D!");
+            }else {
+                System.out.println("Opção " + op + "não existe, animal.");
+            }
+        }while (op >= 3);
     }
 }
